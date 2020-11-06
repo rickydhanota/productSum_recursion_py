db3fd3f00b6d69f6a0a1126d26242d5856ba3ade
Ricky <rickdhanota@gmail.com>
2020-11-06 01:15:02 -0800

    finished

--- productSum.py
+++ productSum.py
@@ -7,5 +7,11 @@
 #Sample input array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
 #Sample output 12 // calculated as: 5 + 2 + 2 * (7-1) + 3 + 2 * (6 + 3) * (13 + 8) + 4
 
-def productSum(array):
-    pass
+def productSum(array, multiplier = 1):
+    sum = 0
+    for element in array:
+        if type(element) is list:
+            sum += productSum(element, multiplier + 1)
+        else:
+            sum += element
+    return sum * multiplier
